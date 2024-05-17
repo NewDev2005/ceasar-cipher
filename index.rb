584afdf07bdad6cbdf2ba5addc34add7e9995d7d
krypto <clayjeansen3@gmail.com>
2024-05-17 11:34:34 +0530

    text wrap from z to a added

--- index.rb
+++ index.rb
@@ -1,10 +1,16 @@
 def ceasar_cipher(str,key)
     str_lowercase = str.downcase
-    str_to_arr =str_lowercase.split(" ")
-    multiDimensional_arr = str_to_arr.map {|chr_to_ascii| chr_to_ascii.bytes.map {|ascii| ascii + key}.map {|ascii| ascii.chr}}
+    str_to_arr = str_lowercase.split(" ")
+    multiDimensional_arr = str_to_arr.map {|chr_to_ascii| chr_to_ascii.bytes.map do|ascii| 
+        if (ascii + key) > 122
+            (ascii + key) - 122 + 96
+        else
+            ascii + key
+        end
+    end.map {|ascii| ascii.chr}}
     cipher_str = multiDimensional_arr.map{|arr| arr.join("")}.join(" ")
     return cipher_str
 end
 
- string = ceasar_cipher("heylo", 4)
+ string = ceasar_cipher("Hey mike is gay", 5)
  puts string
